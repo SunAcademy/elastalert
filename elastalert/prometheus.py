f12e441336a251e08bf9a6287030575e2c913043
Vink <dong@wenqi.us>
2017-11-15 11:48:33 +0800

    add label support

--- elastalert/prometheus.py
+++ elastalert/prometheus.py
@@ -10,14 +10,20 @@ class Prometheus:
 
     def add_metrics(self, rule, data):
         name = rule["name"]
-        g = self.get_metircs(name)
+        g = self.get_metircs(name, labels=rule["labels"])
+        g = self.add_labels(g, rule["labels"])
         if data is None:
             return g.set(0)
         g.set(len(data))
 
-    def get_metircs(self, name="None", describe="This is generate from elastalert"):
+    def add_labels(self, metrics, x={}):
+        m = metrics
+        m = m.labels(*x.values())
+        return m
+
+    def get_metircs(self, name="None", describe="This is generate from elastalert", labels={}):
         if name in self.metrics:
             return self.metrics[name]
-        g = Gauge(name, describe)
+        g = Gauge(name, describe, labelnames=labels.keys())
         self.metrics[name] = g
         return g
