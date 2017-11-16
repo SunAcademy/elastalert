319d9be79216f9c905f76dca91cd702e4cf27d57
Vink <dong@wenqi.us>
2017-11-16 17:27:41 +0800

    add modify tag and prometheus aggregation support

--- elastalert/prometheus.py
+++ elastalert/prometheus.py
@@ -1,5 +1,5 @@
 from prometheus_client import start_http_server, Summary
-from prometheus_client import Gauge, CollectorRegistry
+from prometheus_client import Counter, CollectorRegistry
 
 
 class Prometheus:
@@ -11,19 +11,45 @@ class Prometheus:
     def add_metrics(self, rule, data):
         name = rule["name"]
         g = self.get_metircs(name, labels=rule["labels"])
-        g = self.add_labels(g, rule["labels"])
-        if data is None:
-            return g.set(0)
-        g.set(len(data))
+        if rule.get("aggregation_query_element"):
+            self.set_aggregation_values(g, data=data, r=rule)
+        else:
+            g = self.add_labels(g, data=data and data[0] or {}, r=rule)
+            g.inc(len(data))
 
-    def add_labels(self, metrics, x={}):
-        m = metrics
-        m = m.labels(*x.values())
+    def set_aggregation_values(self, metric, data={}, r={}):
+        if data:
+            ex_bucket_aggs = data.values() and data.values()[0] or False
+            if not ex_bucket_aggs:
+                return True
+            bucket_aggs = ex_bucket_aggs.get("bucket_aggs")
+            if not bucket_aggs:
+                return True
+            buckets = bucket_aggs.get("buckets")
+            if not buckets:
+                return True
+            for bucket in buckets:
+                m = self.add_labels(metric, data=bucket, r=r)
+                m.inc(bucket.get("doc_count", 0))
+        else:
+            m = self.add_labels(metric, data, r)
+            m.inc(0)
+
+    def add_labels(self, metric, data={}, r={}):
+        m = metric
+        l = r["labels"]
+        v = []
+        for x in l:
+            if l[x][:2] == "$.":
+                v.append(data.get(l[x][2:], "/"))
+            else:
+                v.append(l[x])
+        m = m.labels(*v)
         return m
 
     def get_metircs(self, name="None", describe="This is generate from elastalert", labels={}):
         if name in self.metrics:
             return self.metrics[name]
-        g = Gauge(name, describe, labelnames=labels.keys())
+        g = Counter(name, describe, labelnames=labels.keys())
         self.metrics[name] = g
         return g
